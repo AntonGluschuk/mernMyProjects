9014f245bde900cd0d72f30472abe0fa1519e96f
Anton Gluschuk <32397351+AntonGluschuk@users.noreply.github.com>
2021-02-06 18:52:19 +0200

    feat: form markup/initial patch post

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -6,7 +6,7 @@ import { useDispatch } from "react-redux";
 import useStyles from "./styles";
 import { sendPost } from "../Posts/store/postsSlice";
 
-const Form = () => {
+const Form = ({ currentId, setCurrentId }) => {
   const [postData, setPostData] = useState({
     creator: "",
     title: "",
--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,4 +1,5 @@
 import express from "express";
+import mongoose from "mongoose";
 
 import PostMessage from "../models/postMessage.js";
 
@@ -7,8 +8,6 @@ const router = express.Router();
 export const getPosts = async (req, res) => {
   try {
     const postMessages = await PostMessage.find();
-
-    console.log(postMessages);
 
     res.status(200).json(postMessages);
   } catch (error) {
@@ -28,6 +27,20 @@ export const createPost = async (req, res) => {
   } catch (error) {
     res.status(409).json({ message: error.message });
   }
+};
+
+export const updatePost = async (req, res) => {
+  const { id: _id } = req.params;
+  const post = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(_id))
+    return res.status(404).send("No post with that id");
+
+  const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {
+    new: true,
+  });
+
+  res.json(updatedPost);
 };
 
 export default router;
